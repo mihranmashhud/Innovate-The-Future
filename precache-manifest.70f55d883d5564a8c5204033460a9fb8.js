b27fe3ee82a1b6c0e8579fe060ebbaf8ca9ac99e
mihranmashhud <mihranmashhud@gmail.com>
2018-10-17 03:44:28 -0400

    Updates

--- precache-manifest.70f55d883d5564a8c5204033460a9fb8.js
+++ precache-manifest.70f55d883d5564a8c5204033460a9fb8.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-300italic.d69924b9.woff2"
   },
   {
-    "revision": "fa8cd660edee5a1b373b",
+    "revision": "8f7a4d9bd1dbbb3fd505",
     "url": "/Innovate-The-Future/static/css/main.0737d465.chunk.css"
   },
   {
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-300.55536c8e.woff2"
   },
   {
-    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-400.5d4aeb4e.woff2"
-  },
-  {
     "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
     "url": "/Innovate-The-Future/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-400.5d4aeb4e.woff2"
   },
   {
     "revision": "037d830416495def72b7881024c14b7b",
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-100italic.d704bb3d.woff"
   },
   {
-    "revision": "fa8cd660edee5a1b373b",
-    "url": "/Innovate-The-Future/static/js/main.fa8cd660.chunk.js"
+    "revision": "8f7a4d9bd1dbbb3fd505",
+    "url": "/Innovate-The-Future/static/js/main.8f7a4d9b.chunk.js"
   },
   {
     "revision": "a1471d1d6431c893582a5f6a250db3f9",
@@ -72,36 +72,32 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-300italic.210a7c78.woff"
   },
   {
-    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.d8bcbe72.woff2"
-  },
-  {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
     "url": "/Innovate-The-Future/static/media/roboto-latin-400.bafb105b.woff"
-  },
-  {
-    "revision": "de8b7431b74642e830af4d4f4b513ec9",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-500.de8b7431.woff"
-  },
-  {
-    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-500italic.ffcc050b.woff"
-  },
-  {
-    "revision": "510dec37fa69fba39593e01a469ee018",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-500italic.510dec37.woff2"
-  },
-  {
-    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-700italic.010c1aee.woff2"
   },
   {
     "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
     "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.9680d5a0.woff"
   },
   {
-    "revision": "846d1890aee87fde5d8ced8eba360c3a",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-700italic.846d1890.woff"
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-700italic.010c1aee.woff2"
   },
   {
     "revision": "cf6613d1adf490972c557a8e318e0868",
@@ -112,6 +108,10 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-900.8c2ade50.woff"
   },
   {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
     "revision": "7b770d6c53423deb1a8e49d3c9175184",
     "url": "/Innovate-The-Future/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
@@ -120,7 +120,7 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/css/1.48569963.chunk.css"
   },
   {
-    "revision": "4f37654748e147e747227371c0e7b57b",
+    "revision": "2acc4cbc5f93318dc09f886e0ce684f3",
     "url": "/Innovate-The-Future/index.html"
   }
 ];
