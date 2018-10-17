53bf97f073439b6eeddf6d5b240243cba3e519a6
mihranmashhud <mihranmashhud@gmail.com>
2018-10-17 03:38:11 -0400

    a few changes to get gh-pages working.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
