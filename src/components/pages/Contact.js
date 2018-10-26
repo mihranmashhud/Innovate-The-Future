750b383e78edb6d7ca9a3434d9afe6be41397f8b
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-10-26 08:58:42 -0400

    Margin fix for contact us

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { withStyles } from "@material-ui/core/styles"
-import Card from "@material-ui/core/Card"
-import CardActions from "@material-ui/core/CardActions"
-import CardContent from "@material-ui/core/CardContent"
-import Button from "@material-ui/core/Button"
-import Typography from "@material-ui/core/Typography"
-import IconButton from "@material-ui/core/IconButton"
-import CssBaseline from "@material-ui/core/CssBaseline"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Instagram from "mdi-material-ui/Instagram"
-import Facebook from "mdi-material-ui/Facebook"
-import Twitter from "mdi-material-ui/Twitter"
-import Snapchat from "mdi-material-ui/Snapchat"
+import Instagram from "mdi-material-ui/Instagram";
+import Facebook from "mdi-material-ui/Facebook";
+import Twitter from "mdi-material-ui/Twitter";
+import Snapchat from "mdi-material-ui/Snapchat";
 
 const styles = {
   card: {
     maxWidth: 500,
     width: "90%",
-    margin: 50
+    margin: "auto",
+    marginTop: "20px"
   },
   title: {
     fontSize: 60
@@ -29,17 +30,21 @@ const styles = {
   pos1: {
     marginBottom: 25
   }
-}
+};
 
 function Contact(props) {
-  const { classes } = props
+  const { classes } = props;
 
   return (
     <div className={classes.root} align="center">
       <CssBaseline />
       <Card className={classes.card}>
         <CardContent>
-          <Typography className={classes.title} color="textPrimary" gutterBottom>
+          <Typography
+            className={classes.title}
+            color="textPrimary"
+            gutterBottom
+          >
             Contact Information
           </Typography>
           <Typography className={classes.pos} color="textPrimary">
@@ -65,26 +70,42 @@ function Contact(props) {
           </Typography>
 
           <div>
-            <IconButton title="Visit our Facebook Page" color="white" href="http://www.facebook.com">
+            <IconButton
+              title="Visit our Facebook Page"
+              color="white"
+              href="http://www.facebook.com"
+            >
               <Facebook />
             </IconButton>
 
-            <IconButton title="See our Instagram" color="white" href="https://www.instagram.com/innovatethefuture/">
+            <IconButton
+              title="See our Instagram"
+              color="white"
+              href="https://www.instagram.com/innovatethefuture/"
+            >
               <Instagram />
             </IconButton>
 
-            <IconButton title="See our Twitter" color="white" href="http://www.twitter.com">
+            <IconButton
+              title="See our Twitter"
+              color="white"
+              href="http://www.twitter.com"
+            >
               <Twitter />
             </IconButton>
 
-            <IconButton title="See our Snapchat" color="white" href="http://www.snapchat.com">
+            <IconButton
+              title="See our Snapchat"
+              color="white"
+              href="http://www.snapchat.com"
+            >
               <Snapchat />
             </IconButton>
           </div>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
 
-export default withStyles(styles)(Contact)
+export default withStyles(styles)(Contact);
