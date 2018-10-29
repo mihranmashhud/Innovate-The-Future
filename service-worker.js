57fd11618e84c8dacc00cae69dfc4fd963be9a4c
mihranmashhud <mihranmashhud@gmail.com>
2018-10-28 21:42:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Innovate-The-Future/precache-manifest.a93e8d98786a1c263c8b3ba8d83759b1.js"
+  "/Innovate-The-Future/precache-manifest.9a45e7ba6f9fc43dd1bd670ada1ac3f2.js"
 );
 
 workbox.clientsClaim();
