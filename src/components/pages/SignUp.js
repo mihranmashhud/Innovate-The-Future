02f8075f15a49c892a1fa363aa205cfe7fa30de2
mihranmashhud <mihranmashhud@gmail.com>
2018-10-28 21:36:41 -0400

    Work done for sign in and sign up with email and password

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,4 +1,4 @@
-
+import React, { Component } from "react"
 import Button from "@material-ui/core/Button"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import FormControl from "@material-ui/core/FormControl"
@@ -6,9 +6,12 @@ import Input from "@material-ui/core/Input"
 import InputLabel from "@material-ui/core/InputLabel"
 import Paper from "@material-ui/core/Paper"
 import PropTypes from "prop-types"
-import React from "react"
 import Typography from "@material-ui/core/Typography"
 import withStyles from "@material-ui/core/styles/withStyles"
+import StyledLink from "../Styled-Link"
+import { signUpWithEmailAndPassword } from "../../actions"
+import { withRouter } from "react-router-dom"
+import { connect } from "react-redux"
 
 const styles = theme => ({
   layout: {
@@ -34,53 +37,143 @@ const styles = theme => ({
     backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: "100%", 
+    width: "100%",
     marginTop: theme.spacing.unit
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3
   },
   signUp: {
     marginTop: theme.spacing.unit * 2
-  }
+  },
+  inputRoot: {
+    color: theme.palette.secondary.main,
+    "&$inputFocused": {
+      color: theme.palette.secondary.main
+    }
+  },
+  inputFocused: {}
 })
 
-function SignUp(props) {
-  const { classes } = props
+class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: "",
+      confirmation: ""
+    }
+  }
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Enter Your Email Address:</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Enter a Password</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Confirm your Password</InputLabel>
-              <Input name="password-confirmation" type="password" id="password-confirmation" autoComplete="current-password" />
-            </FormControl>
-            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+  handleChange = id => event => {
+    event.persist()
+    this.setState({
+      [id]: event.target.value
+    })
+  }
+
+  signUpWithEmailAndPassword() {
+    console.log(this.state)
+    const { email, password, confirmation } = this.state
+    if (password === confirmation) {
+      this.signUpWithEmailAndPassword(email, password)
+    }
+  }
+
+  render() {
+    const { classes } = this.props
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h5">
               Sign Up
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    </React.Fragment>
-  )
+            </Typography>
+
+            <form className={classes.form}>
+              <FormControl margin="normal" fullWidth>
+                <InputLabel
+                  htmlFor="email"
+                  FormLabelClasses={{
+                    root: classes.inputRoot,
+                    focused: classes.inputFocused
+                  }}
+                >
+                  Email Address
+                </InputLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={this.handleChange("email")}
+                  classes={{
+                    root: classes.inputRoot
+                  }}
+                />
+              </FormControl>
+
+              <FormControl margin="normal" fullWidth>
+                <InputLabel
+                  htmlFor="password"
+                  FormLabelClasses={{
+                    root: classes.inputRoot,
+                    focused: classes.inputFocused
+                  }}
+                >
+                  Password
+                </InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={this.handleChange("password")}
+                  classes={{
+                    root: classes.inputRoot
+                  }}
+                />
+              </FormControl>
+              <FormControl margin="normal" fullWidth>
+                <InputLabel
+                  htmlFor="password-confirm"
+                  FormLabelClasses={{
+                    root: classes.inputRoot
+                  }}
+                >
+                  Confirm Password
+                </InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password-confirm"
+                  autoComplete="current-password"
+                  onChange={this.handleChange("confirmation")}
+                  classes={{
+                    root: classes.inputRoot
+                  }}
+                />
+              </FormControl>
+
+              <Button type="submit" fullWidth variant="contained" color="secondary" className={classes.signUp} onClick={this.signUpWithEmailAndPassword}>
+                <StyledLink to="/Log-In">Sign Up</StyledLink>
+              </Button>
+            </form>
+          </Paper>
+        </main>
+      </React.Fragment>
+    )
+  }
 }
 
 SignUp.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(SignUp)
+export default withRouter(
+  connect(
+    null,
+    {
+      signUpWithEmailAndPassword
+    }
+  )(withStyles(styles)(SignUp))
+)
