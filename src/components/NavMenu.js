86e85bc84344cb30b439d7701ff5f19d90e86aae
Mihran Mashhud <mihranmashhud@gmail.com>
2018-11-30 19:45:46 -0500

    About page made

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,81 +1,85 @@
-import React from "react"
-import Menu from "@material-ui/core/Menu"
-import MenuItem from "@material-ui/core/MenuItem"
-import IconButton from "@material-ui/core/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
-import StyledLink from "./Styled-Link"
-import { connect } from "react-redux"
-import { signOut } from "../actions"
-import CloseIcon from "@material-ui/icons/Close"
-import Snackbar from "@material-ui/core/Snackbar"
+import React from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import StyledLink from "./Styled-Link";
+import { connect } from "react-redux";
+import { signOut } from "../actions";
+import CloseIcon from "@material-ui/icons/Close";
+import Snackbar from "@material-ui/core/Snackbar";
 
 class NavMenu extends React.Component {
   state = {
     anchorEl: null,
     hasSignedOut: false
-  }
+  };
 
   handleClick = event => {
-    this.setState({ anchorEl: event.currentTarget })
-  }
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
   handleClose = () => {
-    this.setState({ anchorEl: null })
-  }
+    this.setState({ anchorEl: null });
+  };
 
   handleSnackbarClose = () => {
-    this.setState({ hasSignedOut: false })
-  }
+    this.setState({ hasSignedOut: false });
+  };
 
   signOut = () => {
-    this.handleClose()
-    this.props.signOut()
-    this.setState({ hasSignedOut: true })
-  }
+    this.handleClose();
+    this.props.signOut();
+    this.setState({ hasSignedOut: true });
+  };
 
   render() {
-    const { anchorEl, hasSignedOut } = this.state
-    let logOut
-    let logIn
-    let apply
+    const { anchorEl, hasSignedOut } = this.state;
+    let logOut;
+    let logIn;
+    let apply;
 
     if (this.props.authenticated) {
       apply = (
-        <StyledLink to="/Apply">
+        <StyledLink to='/Apply'>
           <MenuItem onClick={this.handleClose}>Your Application</MenuItem>
         </StyledLink>
-      )
-      logOut = <MenuItem onClick={this.signOut}>Sign Out</MenuItem>
+      );
+      logOut = <MenuItem onClick={this.signOut}>Sign Out</MenuItem>;
     } else {
       logIn = (
-        <StyledLink to="/Log-In">
+        <StyledLink to='/Log-In'>
           <MenuItem onClick={this.handleClose}>Log In</MenuItem>
         </StyledLink>
-      )
+      );
     }
 
     return (
       <div>
-        <IconButton aria-owns={anchorEl ? "nav-menu" : null} aria-haspopup="true" color="secondary" onClick={this.handleClick}>
+        <IconButton
+          aria-owns={anchorEl ? "nav-menu" : null}
+          aria-haspopup='true'
+          color='secondary'
+          onClick={this.handleClick}>
           <MenuIcon />
         </IconButton>
-        <Menu id="nav-menu" color="inherit" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
+        <Menu id='nav-menu' color='inherit' anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
           {logIn}
           {apply}
           {logOut}
-          <StyledLink to="/About-Us">
-            <MenuItem onClick={this.handleClose}>About Us</MenuItem>
+          <StyledLink to='/About'>
+            <MenuItem onClick={this.handleClose}>About</MenuItem>
           </StyledLink>
 
-          <StyledLink to="/Event-Scope">
+          <StyledLink to='/Event-Scope'>
             <MenuItem onClick={this.handleClose}>Event Scope</MenuItem>
           </StyledLink>
 
-          <StyledLink to="/Example-Projects">
+          <StyledLink to='/Example-Projects'>
             <MenuItem onClick={this.handleClose}>Example Projects</MenuItem>
           </StyledLink>
 
-          <StyledLink to="/Contact-Us">
+          <StyledLink to='/Contact-Us'>
             <MenuItem onClick={this.handleClose}>Contact Us</MenuItem>
           </StyledLink>
         </Menu>
@@ -90,25 +94,25 @@ class NavMenu extends React.Component {
           ContentProps={{
             "aria-describedby": "message-id"
           }}
-          message={<span id="message-id">You have been successfully signed out</span>}
+          message={<span id='message-id'>You have been successfully signed out</span>}
           action={[
-            <IconButton key="close" aria-label="Close" color="secondary" onClick={this.handleSnackbarClose}>
+            <IconButton key='close' aria-label='Close' color='secondary' onClick={this.handleSnackbarClose}>
               <CloseIcon />
             </IconButton>
           ]}
         />
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps(state) {
   return {
     authenticated: Boolean(state.auth)
-  }
+  };
 }
 
 export default connect(
   mapStateToProps,
   { signOut }
-)(NavMenu)
+)(NavMenu);
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,86 @@
 import React, { Component } from "react";
-import { CssBaseline, Typography, Grid } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { CssBaseline, Typography, Grid, withStyles, Card, CardMedia } from "@material-ui/core";
 import { Paper } from "@material-ui/core";
+import { Parallax } from "react-parallax";
+import Image from "material-ui-image";
+
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    display: "block",
+    minWidth: "70%",
+    maxWidth: "90%",
+    width: "600px",
+    margin: "auto",
+    marginTop: "20px"
+  },
+  paper: {
+    paddingTop: "20px",
+    paddingBottom: "20px"
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: "20px"
+  },
+  text: {
+    fontSize: "20px",
+    margin: "20px",
+    height: "100%",
+    width: "auto",
+    textAlign: "left"
+  },
+  card: {
+    minWidth: "20vh",
+    maxWidth: "100%",
+    height: "100%"
+  },
+  image: {
+    height: "100%"
+  }
+});
 
 class About extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <main>
+      <main className={classes.layout}>
         <CssBaseline>
-          <Paper>
-            <Grid container spacing={24}>
-              <Grid item xs={12}>
-                <Paper>
-                  <img src={require("./../../assets/developers-image.jpg")} height={100} />
-                </Paper>
-              </Grid>
-            </Grid>
+          <Paper className={classes.paper}>
+            <Typography variant='h2' className={classes.title} color='secondary'>
+              About The Competition
+            </Typography>
+            <Parallax bgImage={require("./../../assets/vr-image.jpeg")} bgWidth={"100%"} strength={200}>
+              <div style={{ height: "300px" }} />
+            </Parallax>
+            <Typography variant='body1' className={classes.text}>
+              The IF competition aims to help students use their entrepreneurial and computer science skills to help
+              create meaningful software products that have real world applications. The competition models what may be
+              expected of individuals in their future professions by introducing two disciplines that are used in unison
+              within the business and computer science careers.
+            </Typography>
+            <Parallax bgImage={require("./../../assets/developers-image.jpg")} bgWidth={"100%"} strength={200}>
+              <div style={{ height: "300px" }} />
+            </Parallax>
+            <Typography variant='body1' className={classes.text}>
+              As an IF candidate, you will collaborate in small groups of 2-4 people to develop your own software
+              product. Working in your groups, you will address an issue from today’s society to solve using said
+              product. Candidates of any grade and experience level are welcomed to IF. There are no limitations on the
+              complexity or skills you use for your product, even the programming language is up to you. The software
+              you create to address your problem will be due several weeks before the competition date, at which point
+              it will be evaluated by our panel of judges..
+            </Typography>
+            <Parallax bgImage={require("./../../assets/business-man-image.jpg")} bgWidth={"100%"} strength={200}>
+              <div style={{ height: "300px" }} />
+            </Parallax>
+            <Typography variant='body1' className={classes.text}>
+              On the day of the competition, you will have the opportunity to show us your entrepreneurial skills. In 15
+              minutes, you have to convince a mock panel of investors of the value and use of your product and why your
+              product is worth investing in. Your team will present a short business pitch, explaining the product’s
+              characteristics and your target market, and establishing a marketing plan outlining the future for your
+              product. What are you waiting for? Sign up, and become the ones to innovate the future!!
+            </Typography>
           </Paper>
         </CssBaseline>
       </main>
@@ -22,4 +88,8 @@ class About extends Component {
   }
 }
 
-export default About;
+About.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(About);
