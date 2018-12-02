491a69128f75ebd219825204a455189fc449c97a
Mihran Mashhud <mihranmashhud@gmail.com>
2018-12-01 19:15:53 -0500

    updated firebase app fetching

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,31 +1,31 @@
-import * as firebase from "firebase"
-import applicationModel from "../models/application"
+import * as firebase from "firebase";
+import applicationModel from "../models/application";
 
-import { FirebaseConfig } from "../config/keys"
-firebase.initializeApp(FirebaseConfig)
+import { FirebaseConfig } from "../config/keys";
+firebase.initializeApp(FirebaseConfig);
 
-const database = firebase.database()
+const database = firebase.database();
 
-export const authRef = firebase.auth()
+export const authRef = firebase.auth();
 
 export const getApplication = user => {
-  let application = database.ref(`/${user.uid}`).once("value")
-  return application
-}
+  let application = database.ref(`/${user.uid}`).once("value");
+  return application;
+};
 
 export const updateApplication = (user, questions, submitted) => {
-  let model = applicationModel(user.uid, user.email, questions, submitted)
-  return database.ref("/" + user.uid).set(model)
-}
+  let model = applicationModel(user.uid, user.email, questions, submitted);
+  return database.ref(`/applications/${user.uid}`).set(model);
+};
 
 export const submitApplication = (user, questions) => {
-  let model = applicationModel(user.uid, user.email, questions, true)
-  return database.ref(`/${user.uid}`).set(model)
-}
+  let model = applicationModel(user.uid, user.email, questions, true);
+  return database.ref(`/applications/${user.uid}`).set(model);
+};
 
 export const provider = {
   google: new firebase.auth.GoogleAuthProvider(),
   github: new firebase.auth.GithubAuthProvider(),
   facebook: new firebase.auth.FacebookAuthProvider(),
   twitter: new firebase.auth.TwitterAuthProvider()
-}
+};
