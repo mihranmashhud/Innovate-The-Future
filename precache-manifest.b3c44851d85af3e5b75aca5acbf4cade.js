d3aebf6a34f2310d430ad27a94eaa53a827afd56
Mihran Mashhud <mihranmashhud@gmail.com>
2018-12-08 00:22:51 -0500

    Updates

--- precache-manifest.b3c44851d85af3e5b75aca5acbf4cade.js
+++ precache-manifest.b3c44851d85af3e5b75aca5acbf4cade.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-400.5d4aeb4e.woff2"
   },
   {
-    "revision": "c399cc827f9924a7963a",
+    "revision": "7b444d785be9e8c04d8a",
     "url": "/Innovate-The-Future/static/css/main.ceb97a09.chunk.css"
   },
   {
@@ -36,12 +36,12 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/city-image-1.25a96dcd.png"
   },
   {
-    "revision": "987b84570ea69ee660455b8d5e91f5f1",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-100.987b8457.woff2"
-  },
-  {
     "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
     "url": "/Innovate-The-Future/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-100.987b8457.woff2"
   },
   {
     "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
@@ -56,20 +56,20 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-300.55536c8e.woff2"
   },
   {
-    "revision": "a1471d1d6431c893582a5f6a250db3f9",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-300.a1471d1d.woff"
-  },
-  {
     "revision": "d69924b98acd849cdeba9fbff3f88ea6",
     "url": "/Innovate-The-Future/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-300.a1471d1d.woff"
   },
   {
     "revision": "210a7c781f5a354a0e4985656ab456d9",
     "url": "/Innovate-The-Future/static/media/roboto-latin-300italic.210a7c78.woff"
   },
   {
-    "revision": "c399cc827f9924a7963a",
-    "url": "/Innovate-The-Future/static/js/main.c399cc82.chunk.js"
+    "revision": "7b444d785be9e8c04d8a",
+    "url": "/Innovate-The-Future/static/js/main.7b444d78.chunk.js"
   },
   {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
@@ -80,12 +80,12 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
-    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.9680d5a0.woff"
-  },
-  {
     "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
     "url": "/Innovate-The-Future/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-400italic.9680d5a0.woff"
   },
   {
     "revision": "de8b7431b74642e830af4d4f4b513ec9",
@@ -100,12 +100,12 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-500italic.ffcc050b.woff"
   },
   {
-    "revision": "037d830416495def72b7881024c14b7b",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-700.037d8304.woff2"
-  },
-  {
     "revision": "cf6613d1adf490972c557a8e318e0868",
     "url": "/Innovate-The-Future/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-700.037d8304.woff2"
   },
   {
     "revision": "010c1aeee3c6d1cbb1d5761d80353823",
@@ -116,10 +116,6 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-700italic.846d1890.woff"
   },
   {
-    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
-    "url": "/Innovate-The-Future/static/media/roboto-latin-900.19b7a0ad.woff2"
-  },
-  {
     "revision": "8c2ade503b34e31430d6c98aa29a52a3",
     "url": "/Innovate-The-Future/static/media/roboto-latin-900.8c2ade50.woff"
   },
@@ -128,11 +124,15 @@ self.__precacheManifest = [
     "url": "/Innovate-The-Future/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
   {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/Innovate-The-Future/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
     "revision": "1aae46ffc2ba7a8d506d",
     "url": "/Innovate-The-Future/static/css/1.1ca39378.chunk.css"
   },
   {
-    "revision": "84490b4a4eb9575e436922027dc01f47",
+    "revision": "5fe068d9ef6e9df7410d439316708fcf",
     "url": "/Innovate-The-Future/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Innovate-The-Future/precache-manifest.48f15540712812b44b9025d5583847a6.js"
+  "/Innovate-The-Future/precache-manifest.b3c44851d85af3e5b75aca5acbf4cade.js"
 );
 
 workbox.clientsClaim();
