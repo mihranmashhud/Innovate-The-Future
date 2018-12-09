a86e413ace3eb42b747db7d43f5b045f22303604
Mihran Mashhud <mihranmashhud@gmail.com>
2018-12-09 16:46:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "if-innovatethefuture.ca/precache-manifest.91fb8a138fef0a9be16085129b9974d7.js"
+  "/precache-manifest.0ae24110d3abcf4e874c9c531f9c5824.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("if-innovatethefuture.ca/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
