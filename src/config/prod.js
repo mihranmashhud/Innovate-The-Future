6193fbefe9ddae7ca4940139dcf7c4e9f03b7779
Mihran Mashhud <mihranmashhud@localhost.localdomain>
2018-12-22 21:15:59 -0600

    added exemplar links

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBJBco_oXlcJrfDRMRT-6yxTCja6OSZ5R8",
+    authDomain: "if-website-9d3a1.firebaseapp.com",
+    databaseURL: "https://if-website-9d3a1.firebaseio.com",
+    projectId: "if-website-9d3a1",
+    storageBucket: "if-website-9d3a1.appspot.com",
+    messagingSenderId: "768679008713"
+}
